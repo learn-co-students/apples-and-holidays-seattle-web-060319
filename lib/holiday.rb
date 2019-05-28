22f73c9c901f442b25b5b0dc0ebeda2ccb829cb6
Danie Martinoli <daniechris5@gmail.com>
2019-05-28 14:20:24 -0700

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,7 +49,6 @@ def all_supplies_in_holidays(holiday_hash)
         puts "  #{holiday.to_s.split("_").map {|i| i.capitalize}.join (" ")}: #{item.join(", ")}"
       end
    end
-  all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
