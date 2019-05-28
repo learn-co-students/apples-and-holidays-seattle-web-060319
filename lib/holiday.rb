89e5841d2194ef7c70aa9186e22aeb4dc796f123
Danie Martinoli <daniechris5@gmail.com>
2019-05-28 12:37:33 -0700

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -23,9 +23,6 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-    #holiday_hash. each do |key, value|
-    #  holiday_hash[:winter][key] = [supply]
-    #end
   holiday_hash[:winter][:christmas] = [supply]
   holiday_hash[:winter][:new_years] = [supply]
 end
